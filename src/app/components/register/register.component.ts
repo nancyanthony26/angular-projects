f29fc0a784ce9fba50089226676791fc7b151afc
nancyanthony26 <nancyanthony26@gmail.com>
2024-05-13 17:41:35 +0530

    first commit

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit,  } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { User } from 'src/app/interfaces/auth';
+import { AuthService } from 'src/app/services/auth.service';
+import { passwordMatchValidator } from 'src/app/shared/password-match.directive';
+import { FormGroup,FormControl,ReactiveFormsModule} from '@angular/forms';
+
+interface City {
+  name: string;
+  code: string;
+}
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'] 
+})
+
+export class RegisterComponent implements OnInit{
+[x: string]: any;
+
+cities: City[] | undefined;
+    
+formGroup: FormGroup | any;
+
+
+ngOnInit() {
+    this.cities = [
+        { name: 'Chennai', code: 'CH' },
+        { name: 'Mumbai', code: 'MI' },
+        { name: 'Bangalore', code: 'BLE' },
+        { name: 'Hyderbad', code: 'HYD' },
+        { name: 'Delhi', code: 'DH' }
+    ];
+
+    this.formGroup = new FormGroup({
+        selectedCity: new FormControl<City | null>(null)
+    });
+}
+
+  
+
+ 
+
+  registerForm = this.fb.group({
+    fullName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+(?: [a-zA-Z]+)*$/)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+    dateOfBirth: ['null', Validators.required],
+    gender:['',Validators.required],
+    city:['',Validators.required]
+  }, {
+    validators: passwordMatchValidator
+  })
+
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private messageService: MessageService,
+    private router: Router
+  ) { }
+
+  get fullName() {
+    return this.registerForm.controls['fullName'];
+  }
+
+  get email() {
+    return this.registerForm.controls['email'];
+  }
+
+  get password() {
+    return this.registerForm.controls['password'];
+  }
+
+  get confirmPassword() {
+    return this.registerForm.controls['confirmPassword'];
+  }
+
+  submitDetails() {
+    const postData = { ...this.registerForm.value };
+    delete postData.confirmPassword;
+    this.authService.registerUser(postData as User).subscribe(
+      response => {
+        console.log(response);
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Register successfully' });
+        this.router.navigate(['login'])
+      },
+      error => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong' });
+      }
+    )
+  }
+
+}
